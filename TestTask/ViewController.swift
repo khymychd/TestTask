1489639d6c9f744d29aaa334b2a09175f6438d5d
Khymych Dmytriy <khymychd@gmail.com>
2020-01-11 22:06:36 +0200

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by KillerBe on 11.01.2020.
+//  Copyright © 2020 Khymychcrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
